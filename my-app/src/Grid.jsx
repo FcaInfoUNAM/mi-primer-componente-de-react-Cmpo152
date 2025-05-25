fe01810abd5ef521c129adb23972b4b251578173
Jorge Pérez Zempoalteca <jorge_zempoalteca@comunidad.unam.mx>
2025-05-25 21:49:57 +0000

    Mi primer componente de React

--- my-app/src/Grid.jsx
+++ my-app/src/Grid.jsx
@@ -1,3 +1,16 @@
-export function Grid(){
+export function Grid(items){
     /* Escrobe aquí tu código */
+    return (
+        <div className="grid">
+            {items.map((item, index) => (
+                
+                <div key={index} className="card">
+                <h2>{item.title}</h2>
+                <p>{item.ref}</p>
+                </div>
+
+            ))}
+        </div>
+    );
+
 }
